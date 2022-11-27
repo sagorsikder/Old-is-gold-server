4570c8f2eeb51208611e46ba14613534b11c713e
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-27 22:47:11 +0600

    user save database

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run(){
     try{
 
         const categoryCollection = client.db('oldIsGold').collection('categories');
+        const userCollection = client.db('oldIsGold').collection('user');
 
         app.get('/categories',async(req,res)=>{
 
@@ -46,6 +47,18 @@ async function run(){
        
 
             res.send(categories)
+        })
+        
+        app.post('/users',async(req,res)=>{
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users',async(req,res)=>{
+            const query = {};
+            const users = await userCollection.find(query).toArray();
+            res.send(users);
         })
 
     }
