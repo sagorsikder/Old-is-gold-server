06a1f849c4705e2d79c8b703fce4604dfe3a30a7
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-28 21:10:30 +0600

    completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// dotenv config
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
